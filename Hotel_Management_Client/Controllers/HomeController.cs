dc1afb9687d98c5f13119259b15cef55bfc8ed84
Vu Viet Hoang <hoangvvhe161227@fpt.edu.vn>
2023-11-08 09:34:56 +0700

    update home

--- Hotel_Management_Client/Controllers/HomeController.cs
+++ Hotel_Management_Client/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Hotel_Management_Client.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+
 
 namespace Hotel_Management_Client.Controllers
 {
@@ -13,9 +15,41 @@ namespace Hotel_Management_Client.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        private readonly HttpClient _client;
+
+        public HomeController()
         {
-            return View();
+            _client = new HttpClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            // Fetch the list of floors from the API
+            HttpResponseMessage floorsResponse = await _client.GetAsync("https://localhost:7083/api/Floors");
+
+            if (floorsResponse.IsSuccessStatusCode)
+            {
+                var floorsJson = await floorsResponse.Content.ReadAsStringAsync();
+                var floors = JsonConvert.DeserializeObject<List<HomeViewModel.FloorViewModel>>(floorsJson);
+
+                // Fetch the list of rooms for each floor
+                foreach (var floor in floors)
+                {
+                    HttpResponseMessage roomsResponse = await _client.GetAsync($"https://localhost:7083/api/Rooms/ByFloor/{floor.FloorId}");
+                    if (roomsResponse.IsSuccessStatusCode)
+                    {
+                        var roomsJson = await roomsResponse.Content.ReadAsStringAsync();
+                        floor.Rooms = JsonConvert.DeserializeObject<List<HomeViewModel.RoomViewModel>>(roomsJson);
+                    }
+                }
+
+                return View(floors);
+            }
+
+            // Handle the case where the API request was not successful
+            return View("Error");
+
         }
 
         public IActionResult Privacy()
